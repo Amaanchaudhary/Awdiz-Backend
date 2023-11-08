672d8b67468ebe0af75c376c0e4a652311b41673
Amaanchaudhary <chaudharyamaan982@gmail.com>
2023-11-08 21:02:18 +0530

    update

--- Modals/Product.modals.js
+++ Modals/Product.modals.js
@@ -1,14 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 
 const product = new Schema({
-    name : String,
-    price : Number,
-    category : String,
-    image : String,
-    userId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User"
+    name: String,
+    price: Number,
+    category: String,
+    image: String,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     }
-})
+}, { timestamps: true })
 
-export default mongoose.model("Product" , product);
+export default mongoose.model("Product", product);
--- Routes/Products.routes.js
+++ Routes/Products.routes.js
@@ -1,11 +1,14 @@
 import {Router} from 'express'
-import { addProduct, getllProducts } from '../Controllers/Products.controller.js';
+import { addProduct, getllProducts , getSingleProducts , filterProducts} from '../Controllers/Products.controller.js';
 import { checkUserID } from '../Middleware/AllMiddleware.js';
 
 const router = Router();
 
 router.post("/add-product" , checkUserID , addProduct);
-router.post("/get-all-product" , checkUserID , getllProducts);
+router.get("/get-all-product" ,  getllProducts);
+router.get("/get-single-product" , getSingleProducts);
+router.get("/filter-products", filterProducts)
+
 
 
 export default router;
